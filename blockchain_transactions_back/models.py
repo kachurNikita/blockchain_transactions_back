2a34a695181e216eaa7f6eef8883e1001cf07057
Nikita <ashtonashtonkachur@gmail.com>
2023-02-23 22:40:35 -0500

    change amount field

--- blockchain_transactions_back/models.py
+++ blockchain_transactions_back/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Transaction(models.Model):
     address_from = models.CharField(max_length=65)
     address_to = models.CharField(max_length=65)
-    amount = models.CharField(max_length=20)
+    amount = models.IntegerField()
     transaction_hash = models.CharField(max_length=65, null=True)
